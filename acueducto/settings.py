bb83c9ff1e3d5af86396bd8618f2dbc660368707
Diego Alejandro <bermudezdiegoalejandrou@gmail.com>
2022-12-05 20:53:05 -0500

    actualizacion seeders

--- acueducto/settings.py
+++ acueducto/settings.py
@@ -90,6 +90,10 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': 5432,
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
