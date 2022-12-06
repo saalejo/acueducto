ae1f23668087a387aa909e98160164a3041bff4c
Diego Alejandro <bermudezdiegoalejandrou@gmail.com>
2022-12-05 22:35:46 -0500

    truncate in postgres

--- querys/limpiar.sql
+++ querys/limpiar.sql
@@ -1,4 +1,8 @@
+-- SQLite
 delete from util_control;
 delete from util_subsidio;
 delete from util_consumo;
 delete from util_movimiento;
+
+-- PostgreSQL
+TRUNCATE util_control, util_subsidio, util_consumo, util_movimiento;
