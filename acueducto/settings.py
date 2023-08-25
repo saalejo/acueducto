5ff81bebe916e4ba3605d67eb607d0d8a23699aa
Diego Alejandro <bermudezdiegoalejandrou@gmail.com>
2023-08-25 15:48:34 -0500

    importar exportar
    Signed-off-by: Diego Alejandro <bermudezdiegoalejandrou@gmail.com>

--- acueducto/settings.py
+++ acueducto/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'django_filters',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- util/admin.py
+++ util/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+
+from util.resources import *
 from .models import *
+from import_export.admin import ImportExportModelAdmin
+
+class ConsumoAdmin(ImportExportModelAdmin):
+    resource_classes = [ConsumoResource]
+
+class ClienteAdmin(ImportExportModelAdmin):
+    resource_classes = [ClienteResource]
 
 admin.site.site_header = 'Acueducto'
 admin.site.register(Control)
-admin.site.register(Consumo)
-admin.site.register(Cliente)
+admin.site.register(Consumo, ConsumoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Dispositivo)
 admin.site.register(Elemento)
 admin.site.register(Lectura)
